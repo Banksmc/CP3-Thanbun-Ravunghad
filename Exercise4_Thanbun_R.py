3f17780947bc4d1ad88e55ce5f61d10905956fd4
Banksmc <117836631+Banksmc@users.noreply.github.com>
2022-11-14 12:55:18 +0700

    Exercise4_Thanbun_R
    Variable

---
+++ Exercise4_Thanbun_R.py
@@ -0,0 +1,10 @@
+FE = 67.8
+GB = 77.3
+ICS = 58.9
+CP = 80
+
+print("---Your Score---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to computer System :", ICS)
+print("Computer programming :", CP)
